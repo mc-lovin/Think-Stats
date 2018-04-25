7dcc7bbd22a0e389aa5baf982a66e93bc47dade0
Ashutosh Singla <ashutoshsingla@ymail.com>
2018-04-25 09:38:15 -0400

    Remove index, types are already inferred

--- survey.R
+++ survey.R
@@ -1,33 +1,31 @@
 library(dplyr)
 
 readFWF <- function(filename, fields) {
-  fields <- mutate(fields, gap = start - lag(end, default = 1), cur = end - start + 1)
+  fields <- mutate(fields, gap = start - lag(end, default = 0) - 1, cur = end - start + 1)
   fields$gap = -fields$gap
+
   widths <- as.vector(rbind(fields$gap, fields$cur))
-  print (widths)
   widths <- widths[widths != 0]
   df <- read.fwf(filename, widths = widths)
-  colnames(df) = fields$name
   
-  # TODO cast into type
+  colnames(df) = fields$name
   return (df)
 }
 
-# Create the respondants table
+# Create the pregnancy table
 pregnancy.fields <- data.frame(
-  start <- c(1,22, 56, 57, 59, 275, 277, 278, 284, 423),
-  end <- c(12, 22, 56, 58, 60, 276, 277, 279, 287, 440),
-  name <- c('caseid', 'nbrnaliv', 'babysex', 'birthwgt_lb', 
-           'birthwgt_oz', 'prglength', 'outcome', 'birthord', 'agepreg', 'finalwgt'),
-  classes <- c('int', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'int', 'float')
+  start = c(1,22, 56, 57, 59, 275, 277, 278, 284, 423),
+  end = c(12, 22, 56, 58, 60, 276, 277, 279, 287, 440),
+  name = c('caseid', 'nbrnaliv', 'babysex', 'birthwgt_lb',
+           'birthwgt_oz', 'prglength', 'outcome', 'birthord', 'agepreg', 'finalwgt')
 )
 pregnancy <- readFWF("data/2002FemPreg.dat", pregnancy.fields)
 
+# Create the respondants table
 respondants.fields <- data.frame(
-  start <- c(1),
-  end <- c(12),
-  name <- c('caseid'),
-  type <- c('int')
+  start = c(1),
+  end = c(12),
+  name = c('caseid')
 )
 respondants = readFWF("data/2002FemResp.dat", respondants.fields)
 

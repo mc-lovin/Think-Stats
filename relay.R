486be57995260caac7b5e204fb8d6349d7c77fd5
Ashutosh Singla <ashutoshsingla@ymail.com>
2018-05-02 09:14:38 -0700

    CDF AND PDF for continous variables

---
+++ relay.R
@@ -0,0 +1,35 @@
+library(stringr)
+
+fetchData <- function() {
+  url <- 'http://www.coolrunning.com/results/10/ma/Apr25_27thAn_set1.shtml'
+  lines <- readLines(url)
+  no.records <- length(lines)
+  speeds <- c()
+  
+  for (i in 1:no.records) {
+    record = lines[i]
+    record <- strsplit(record, ' ')[[1]]
+    record <- record[record != '']
+    
+    if (str_count(paste(record[4], record[5], record[6]), ':') < 3) {
+      next
+    }
+    
+    speeds <- c(speed, convertPaceToMPH(record[6]))
+  }
+  return (speeds)
+}
+
+convertPaceToMPH <- function(entry) {
+  # Pace is formated as Minute : Seconds / Mile
+  entry <- strsplit(entry, ':')[[1]]
+  seconds <- as.integer(entry[1]) * 60 + as.integer(entry[2])
+  return ((1.0 / seconds) * 60 * 60)
+}
+
+# Since this is a continuous random distribution, making histogram plots does not make sense
+# Convert this into a probability distribution and plot it
+speeds <- fetchData()
+par(mfrow=c(1,2))
+plot(speeds, dnorm(speeds), type="l", xlab="speed", ylab="probability")
+plot(speeds, pnorm(speeds), type="l", xlab="speed", ylab="cummulative probability")
